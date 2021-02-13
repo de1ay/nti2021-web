cb02c45db13012c35aedca686c5dce2eadc25f9f
Vlad Utkin <vlad@teamfnd.ru>
2021-02-13 07:08:05 +0300

    TODO need rework and fast

--- src/modules/warehouse/Warehouse.vue
+++ src/modules/warehouse/Warehouse.vue
@@ -1,4 +1,5 @@
 <template lang="pug">
+h1.h1 Состояние склада
 div.warehouse
   div.shelf
     div.shelf11.shelf_inter
@@ -31,48 +32,59 @@ export default {
       shelf12: null,
       shelf21: null,
       shelf22: null,
+      timer: null,
     };
   },
   mounted() {
-    axios
-      .get('https://api.offsound.ru/item/')
-      // eslint-disable-next-line no-return-assign
-      .then((response) => {
-        // eslint-disable-next-line camelcase
-        const shelf11 = [];
-        const shelf12 = [];
-        const shelf21 = [];
-        const shelf22 = [];
-        // eslint-disable-next-line no-var,no-plusplus,vars-on-top
-        for (var i = 0; i < 216; i++) {
-          // eslint-disable-next-line no-loop-func
-          const obj = response.data.results.find((o) => o.address === i + 1);
-          if (i < 108) {
-            if (i % 2) {
-              shelf11.push(obj);
+    function timerfunc() {
+      axios
+        .get('https://api.offsound.ru/item/')
+        // eslint-disable-next-line no-return-assign
+        .then((response) => {
+          // eslint-disable-next-line camelcase
+          const shelf11 = [];
+          const shelf12 = [];
+          const shelf21 = [];
+          const shelf22 = [];
+          // eslint-disable-next-line no-var,no-plusplus,vars-on-top
+          for (var i = 0; i < 216; i++) {
+            // eslint-disable-next-line no-loop-func
+            const obj = response.data.results.find((o) => o.address === i + 1);
+            if (i < 108) {
+              if (i % 2) {
+                shelf11.push(obj);
+              } else {
+                shelf12.push(obj);
+              }
             } else {
-              shelf12.push(obj);
-            }
-          } else {
-            // eslint-disable-next-line no-lonely-if
-            if (i % 2) {
-              shelf21.push(obj);
-            } else {
-              shelf22.push(obj);
+              // eslint-disable-next-line no-lonely-if
+              if (i % 2) {
+                shelf21.push(obj);
+              } else {
+                shelf22.push(obj);
+              }
             }
           }
-        }
-        // eslint-disable-next-line camelcase
-        this.shelf11 = shelf11;
-        this.shelf12 = shelf12;
-        this.shelf21 = shelf21;
-        this.shelf22 = shelf22;
-      });
+          // eslint-disable-next-line camelcase
+          this.shelf11 = shelf11;
+          this.shelf12 = shelf12;
+          this.shelf21 = shelf21;
+          this.shelf22 = shelf22;
+        });
+    }
+    this.timer = timerfunc();
+    setInterval(this.timer.bind(this), 1000);
+  },
+  beforeUnmount() {
+    this.timer = null;
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.h1 {
+  font-family: $font-firaSansCondensed;
+}
 .warehouse {
   display: grid;
   grid-template-columns: repeat(2, 1fr);
